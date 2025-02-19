fa2d7a06430453d179d5126a85c8cf45afcc764f
trungduc81 <ductrung081105@gmail.com>
2025-02-19 10:40:50 +0700

    Create BÀI 1. ƯỚC SỐ CHUNG LỚN NHẤT VÀ BỘI SỐ CHUNG NHỎ NHẤT.cpp

---
+++ Chương 1/BÀI 1. ƯỚC SỐ CHUNG LỚN NHẤT VÀ BỘI SỐ CHUNG NHỎ NHẤT.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+#define ll long long
+
+void solve(int a , int b){
+	int gcd = __gcd(a,b) ;
+	ll lcm = a/gcd * b ;
+	cout << gcd << " " << lcm << endl ;
+}
+
+int main()
+{
+	int t ; 
+	cin >> t ; 
+	while(t--){
+		int a , b ; 
+		cin >> a >> b ;
+		solve(a,b) ; 
+	}
+}
