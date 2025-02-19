69f90a711978b4d53081aeed1deacf60c9ee53aa
trungduc81 <ductrung081105@gmail.com>
2025-02-19 19:50:02 +0700

    Create BÀI 6 . SỐ TĂNG GIẢM .cpp

---
+++ Chương 1/BÀI 6 . SỐ TĂNG GIẢM .cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve1(string &s) {
+    int m = s.size();
+    for (int i = 0; i < m - 1; i++) {
+        if (s[i] > s[i + 1]) return 0;
+    }
+    return 1;
+}
+
+int solve2(string &s) {
+    int m = s.size();
+    for (int i = 0; i < m - 1; i++) {
+        if (s[i] < s[i + 1]) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        if (solve1(s) || solve2(s))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
+// do phuc tap O(n) 
